5f5f2735fa262b9986cc42b241727fa890c14161
Aydan Jordan <aydan.jordan@wsu.edu>
2019-02-23 15:56:55 -0800

    .

---
+++ TronBattleRoyale/TronBattleRoyale/Content/Source/Engine/Globals.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TronBattleRoyale
+{
+
+    public delegate void PassObject(object i);
+    public delegate object PassObjectAndReturn(object i);
+
+
+    public class Globals
+    {
+
+        public static ContentManager content;
+        public static SpriteBatch spriteBatch;
+        public static int screenHeight;
+        public static int screenWidth;
+
+        public static GameTime gameTime;
+
+        public static McKeyboard keyboard;
+
+        public static float GetDistance(Vector2 pos, Vector2 target)
+        {
+            return (float)Math.Sqrt(Math.Pow(pos.X - target.X, 2) + Math.Pow(pos.Y - target.Y, 2));
+        }
+
+        public static Vector2 RadialMovement(Vector2 focus, Vector2 pos, float speed)
+        {
+            float dist = Globals.GetDistance(pos, focus);
+
+            if (dist <= speed)
+            {
+                return focus - pos;
+            }
+            else
+            {
+                return (focus - pos) * speed / dist;
+            }
+        }
+
+    }
+}
