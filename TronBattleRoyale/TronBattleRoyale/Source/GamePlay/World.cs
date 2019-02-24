6f352d71ba8a3782981565f9722a11029cc92860
walkerv <walkervela@gmail.com>
2019-02-23 16:56:52 -0800

    Background updated

--- TronBattleRoyale/TronBattleRoyale/Source/GamePlay/World.cs
+++ TronBattleRoyale/TronBattleRoyale/Source/GamePlay/World.cs
@@ -17,10 +17,12 @@ namespace TronBattleRoyale
 
     public class World
     {
-
+        public Basic2d background;
+        public Basic2d backBackground;
         public World()
         {
-            
+            backBackground = new TronBattleRoyale.Basic2d("2d\\Board\\Black", new Vector2(Globals.screenWidth / 2, Globals.screenHeight / 2), new Vector2(Globals.screenWidth, Globals.screenHeight));
+            background = new Basic2d("2d\\Board\\Background", new Vector2(Globals.screenWidth/2, Globals.screenHeight/2 - 75), new Vector2(Globals.screenWidth, Globals.screenHeight - 75));
         }
 
         public virtual void Update()
@@ -30,6 +32,8 @@ namespace TronBattleRoyale
 
         public virtual void Draw()
         {
+            backBackground.Draw();
+            background.Draw();
             
         }
 
